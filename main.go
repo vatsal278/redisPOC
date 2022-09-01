a05885c1469a02cfd95a1bfbccdea5083dbcd076
vatsal278 <vatsalchaturvedi278@gmail.com>
2022-09-01 20:55:55 +0530

    Completed the POC for redis

--- main.go
+++ main.go
@@ -10,34 +10,36 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"io/ioutil"
 	"log"
+	"strconv"
 )
 
 type Post struct {
-	ID    int64  `json:"id"`
+	ID    int    `json:"id"`
 	Title string `json:"title"`
 }
 
-func redisGet() ([]byte, error) {
+func redisGet(key string) ([]byte, error) {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "localhost:9096",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
-	data, err := rdb.Get(context.Background(), "id").Bytes()
+	data, err := rdb.Get(context.Background(), key).Bytes()
 	if err != nil {
-		panic(err)
+		log.Print(err)
 	}
 	return data, err
 }
 func redisSet(i interface{}) error {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
+		Addr:     "localhost:9096",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
+
 	err := rdb.Set(context.Background(), "id", i, 0).Err()
 	if err != nil {
-		panic(err)
+		log.Print(err)
 	}
 	return err
 }
@@ -52,7 +54,7 @@ func main() {
 
 	db, err := sql.Open(dbDriver, connection_string)
 	if err != nil {
-		panic(err.Error())
+		log.Print(err.Error())
 	}
 	db_string := fmt.Sprintf("CREATE SCHEMA IF NOT EXISTS %s", dbName)
 	_, err = db.Exec(db_string)
@@ -64,15 +66,55 @@ func main() {
 
 	db, err = sql.Open(dbDriver, connection_string)
 	if err != nil {
-		panic(err.Error())
+		log.Print(err.Error())
 	}
-	defer db.Close()
 	db_string = fmt.Sprintf("CREATE Table IF NOT EXISTS %s(id int NOT NULL AUTO_INCREMENT, title text, PRIMARY KEY (id));", "posts")
 	_, err = db.Exec(db_string)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 	r := gin.Default()
+	r.GET("/pong/:id", func(c *gin.Context) {
+		var post Post
+		x := c.Param("id")
+		log.Print(x)
+		intVar, err := strconv.Atoi(x)
+		if err != nil {
+			log.Print(err)
+		}
+		data, err := redisGet("id")
+		if err != nil {
+			log.Print(err)
+		}
+		err = json.Unmarshal(data, &post)
+		if err != nil {
+			log.Print(err)
+		}
+		if post.ID != intVar {
+
+			result, err := db.Query("SELECT id, title FROM posts WHERE id = ?", intVar)
+			if err != nil {
+				log.Print(err.Error())
+			}
+			defer result.Close()
+			for result.Next() {
+				err := result.Scan(&post.ID, &post.Title)
+				if err != nil {
+					log.Print(err.Error())
+				}
+			}
+			x, err := json.Marshal(post)
+			err = redisSet(x)
+			if err != nil {
+				log.Print(err)
+			}
+			json.NewEncoder(c.Writer).Encode(post)
+			return
+		}
+		log.Print("cached data")
+		json.NewEncoder(c.Writer).Encode(post)
+
+	})
 	r.POST("/ping", func(c *gin.Context) {
 		var posts Post
 		stmt, err := db.Prepare("INSERT INTO posts(title) VALUES(?)")
@@ -92,46 +134,7 @@ func main() {
 		}
 		json.NewEncoder(c.Writer).Encode(posts)
 	})
-	r.GET("/pong", func(c *gin.Context) {
-		var post Post
-		var request int64
-		//x:= c.Request.URL.Query()
-		body, err := ioutil.ReadAll(c.Request.Body)
-		if err != nil {
-			panic(err.Error())
-		}
+	r.GET("/pon", func(c *gin.Context) {})
 
-		json.Unmarshal(body, &request)
-		data, err := redisGet()
-		if err != nil {
-			panic(err)
-		}
-		err = json.Unmarshal(data, &post)
-		if err != nil {
-			panic(err)
-		}
-		if post.ID != request {
-
-			result, err := db.Query("SELECT id, title FROM posts WHERE id = ?", request)
-			if err != nil {
-				panic(err.Error())
-			}
-			defer result.Close()
-			for result.Next() {
-				err := result.Scan(&post.ID, &post.Title)
-				if err != nil {
-					panic(err.Error())
-				}
-			}
-			err = redisSet(post)
-			if err != nil {
-				panic(err)
-			}
-			json.NewEncoder(c.Writer).Encode(post)
-			return
-		}
-		json.NewEncoder(c.Writer).Encode(post)
-
-	})
 	r.Run(":8086")
 }
