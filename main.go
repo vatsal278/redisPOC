9e3aee9e6fbcbf2d7fe2f539d968b5c05affdf0a
vatsal278 <vatsalchaturvedi278@gmail.com>
2022-09-02 13:19:34 +0530

    Completed the POC for redis

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
@@ -10,39 +9,44 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"io/ioutil"
 	"log"
-	"strconv"
+	"net/http"
+	"time"
 )
 
 type Post struct {
-	ID    int    `json:"id"`
+	ID    string `json:"id"`
 	Title string `json:"title"`
 }
-
-func redisGet(key string) ([]byte, error) {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:9096",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-	data, err := rdb.Get(context.Background(), key).Bytes()
-	if err != nil {
-		log.Print(err)
-	}
-	return data, err
+type opt struct {
+	Addr string
+	// Use the specified Username to authenticate the current connection
+	// with one of the connections defined in the ACL list when connecting
+	// to a Redis 6.0 instance, or greater, that is using the Redis ACL system.
+	Username string
+	// Optional password. Must match the password specified in the
+	// requirepass server configuration option (if connecting to a Redis 5.0 instance, or lower),
+	// or the User Password when connecting to a Redis 6.0 instance, or greater,
+	// that is using the Redis ACL system.
+	Password string
+	// Database to be selected after connecting to the server.
+	DB int
+	// Maximum number of retries before giving up.
+	// Default is 3 retries; -1 (not 0) disables retries.
+	MaxRetries int
+	// Dial timeout for establishing new connections.
+	// Default is 5 seconds.
+	DialTimeout time.Duration
+	// Maximum number of socket connections.
+	// Default is 10 connections per every available CPU as reported by runtime.GOMAXPROCS.
+	PoolSize int
+	// Minimum number of idle connections which is useful when establishing
+	// new connection is slow.
+	MinIdleConns int
+	// Connection age at which client retires (closes) the connection.
+	// Default is to not close aged connections.
 }
-func redisSet(i interface{}) error {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:9096",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
 
-	err := rdb.Set(context.Background(), "id", i, 0).Err()
-	if err != nil {
-		log.Print(err)
-	}
-	return err
-}
+//client args as
 
 func main() {
 	var db *sql.DB
@@ -68,7 +72,7 @@ func main() {
 	if err != nil {
 		log.Print(err.Error())
 	}
-	db_string = fmt.Sprintf("CREATE Table IF NOT EXISTS %s(id int NOT NULL AUTO_INCREMENT, title text, PRIMARY KEY (id));", "posts")
+	db_string = fmt.Sprintf("CREATE Table IF NOT EXISTS %s(id text NOT NULL, title text, PRIMARY KEY (id));", "posts")
 	_, err = db.Exec(db_string)
 	if err != nil {
 		log.Fatal(err.Error())
@@ -77,22 +81,14 @@ func main() {
 	r.GET("/pong/:id", func(c *gin.Context) {
 		var post Post
 		x := c.Param("id")
-		log.Print(x)
-		intVar, err := strconv.Atoi(x)
-		if err != nil {
-			log.Print(err)
-		}
-		data, err := redisGet("id")
-		if err != nil {
-			log.Print(err)
-		}
-		err = json.Unmarshal(data, &post)
-		if err != nil {
-			log.Print(err)
-		}
-		if post.ID != intVar {
 
-			result, err := db.Query("SELECT id, title FROM posts WHERE id = ?", intVar)
+		data, err := redisGet(x)
+		if err != nil {
+			if err != redis.Nil {
+				log.Print(err)
+				return
+			}
+			result, err := db.Query("SELECT id, title FROM posts WHERE id = ?", x)
 			if err != nil {
 				log.Print(err.Error())
 			}
@@ -104,12 +100,17 @@ func main() {
 				}
 			}
 			x, err := json.Marshal(post)
-			err = redisSet(x)
+			err = redisSet(post.ID, x)
 			if err != nil {
 				log.Print(err)
 			}
 			json.NewEncoder(c.Writer).Encode(post)
 			return
+			log.Print()
+		}
+		err = json.Unmarshal(data, &post)
+		if err != nil {
+			log.Print(err)
 		}
 		log.Print("cached data")
 		json.NewEncoder(c.Writer).Encode(post)
@@ -132,9 +133,8 @@ func main() {
 		if err != nil {
 			panic(err.Error())
 		}
-		json.NewEncoder(c.Writer).Encode(posts)
+		c.JSON(http.StatusCreated, posts)
 	})
-	r.GET("/pon", func(c *gin.Context) {})
 
 	r.Run(":8086")
 }
